a6041c55e3062095c2c95ccfc310b5a6f0c79348
Sebastian Andrei Ion <sebastianandrei.ion@gameloft.com>
2019-01-07 13:55:54 +0200

    +first commit

---
+++ conanfile.py
@@ -0,0 +1,20 @@
+from conans import ConanFile, Meson
+
+class HelloConan(ConanFile):
+    name = "ColumnarDataframe"
+    version = "0.1.0"
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "pkg_config" , "json"
+    exports_sources = "*"
+    requires = "catch2/2.4.0@bincrafters/stable"
+        
+    def build(self):
+        meson = Meson(self)
+        meson.configure(
+            source_folder="%s" % self.source_folder,
+            build_folder="build"
+        )
+        meson.build()
+
+    def package_info(self):
+        self.cpp_info.libs = ["ColumnarDataframe"]
